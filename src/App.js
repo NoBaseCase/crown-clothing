cf5016ff4eb6a04de3284a5f6ef125d893951d0f
nobasecase <rollyfuentes.dev@gmail.com>
2024-06-04 16:07:25 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        <div className="category-body-container">
+          <p>image</p>
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        <div className="category-body-container">
+          <p>image</p>
+          <h2>jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        <div className="category-body-container">
+          <p>image</p>
+          <h2>sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        <div className="category-body-container">
+          <p>image</p>
+          <h2>womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        <div className="category-body-container">
+          <p>image</p>
+          <h2>mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
