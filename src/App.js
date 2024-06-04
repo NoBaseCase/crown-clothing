4758a340cc8a71185e5873245026776922c223af
nobasecase <rollyfuentes.dev@gmail.com>
2024-06-04 16:33:55 -0500

    installed yarn and sass configurations

--- src/App.js
+++ src/App.js
@@ -1,45 +1,26 @@
+import "./categories.styles.scss";
+
 const App = () => {
+  const categories = [
+    { id: 1, title: "Keyboards" },
+    { id: 2, title: "Keycaps" },
+    { id: 3, title: "Switches" },
+    { id: 4, title: "Deskmats" },
+    { id: 5, title: "Cables" },
+  ];
   return (
     <div className="categories-container">
-      <div className="category-container">
-        <div className="category-body-container">
-          <p>image</p>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        <div className="category-body-container">
-          <p>image</p>
-          <h2>jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        <div className="category-body-container">
-          <p>image</p>
-          <h2>sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        <div className="category-body-container">
-          <p>image</p>
-          <h2>womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        <div className="category-body-container">
-          <p>image</p>
-          <h2>mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      {categories.map((category) => {
+        return (
+          <div className="category-container">
+            <div className="background-image" />
+            <div className="category-body-container">
+              <h2>{category.title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
