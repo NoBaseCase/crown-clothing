f716f3dc61b1feecf23eee478e43f0cee344764b
nobasecase <rollyfuentes@gmail.com>
2024-06-06 22:45:43 -0500

    added sign in route to base app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from "./routes/home/home.component";
 import Nav from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 import { Routes, Route } from "react-router-dom";
 
 const Shop = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Route path="/" element={<Nav />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
