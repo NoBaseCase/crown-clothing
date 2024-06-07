84f02d70fd57b00efac2616c0f98fe5cb2b1caff
nobasecase <rollyfuentes@gmail.com>
2024-06-06 22:46:21 -0500

    created navigation component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,23 @@
-import "./navigation.component.scss";
-import { Outlet } from "react-router-dom";
+import "./navigation.styles.scss";
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
+
 const Nav = () => {
   return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to='/'>
+          <CrownLogo className="logo" />
+        </Link>
+
+        <div className="nav-links-container">
+          <Link className="nav-link" to='/shop'>Shop</Link>
+          <Link className="nav-link" to='/sign-in'>Sign In</Link>
+        </div>
       </div>
       <Outlet />
-    </div>
+    </Fragment>
   );
 };
 
